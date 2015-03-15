c4d8d623de8843b718fd51f3cc962ef7b0108106
Philip Ong <philipo0@Hotmail.com>
2015-03-15 17:40:21 -0400

    Added scene 1 stuff
    Just some movement for the player

---
+++ Game/Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+    public float movementSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 Velocity = new Vector3(movementSpeed*Time.deltaTime, 0, 0);
+        this.transform.position = this.transform.position + Velocity;
+	}
+}
