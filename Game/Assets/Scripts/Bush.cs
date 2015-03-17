a22e0b6bd4bac3042305648076ce71cdfb207360
Philip Ong <philipo0@Hotmail.com>
2015-03-17 18:58:01 -0400

    Added Cat Image
    The cat image will appear, and started the sound manager

---
+++ Game/Assets/Scripts/Bush.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bush : MonoBehaviour {
+    public bool Fox;
+    SpriteRenderer Image;
+	// Use this for initialization
+	void Start () {
+        Image = this.transform.FindChild("Animal").GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider hit)
+    {
+        //Debug.Log(hit.name);
+        if (hit.gameObject.tag == "Player")
+        {
+            Image.enabled = true;
+        }
+    }
+
+    public void AnimalFound()
+    {
+        Image.enabled = false;
+    }
+}
