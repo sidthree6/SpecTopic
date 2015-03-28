52afb8b9e31f2dc4e8bd30589fa5ffb393a0aaf8
Philip Ong <philipo0@Hotmail.com>
2015-03-28 19:55:51 -0400

    Added a score counter and end round
    there is a counter for the current round, and score.
    there is also a end of round sequence and a image.

--- Game/Assets/Scripts/Player.cs
+++ Game/Assets/Scripts/Player.cs
@@ -1,20 +1,39 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Player : MonoBehaviour {
     public float movementSpeed;
 
+    private int overallCorrect,currentCorrect,currentRound;
+    private Text RoundNum, Score;
+    private Vector3 Velocity;
     private Bush CurrentBush;
-    private bool hitBush;
+    private bool hitBush, moving;
+    private RawImage LevelComplete;
+
 	// Use this for initialization
 	void Start () {
+        moving = true;
         hitBush = false;
+        overallCorrect = 0;
+        currentCorrect = 0;
+        currentRound = 1;
+
+        RoundNum = GameObject.Find("Round").GetComponent<UnityEngine.UI.Text>();
+        Score = GameObject.Find("Score").GetComponent<UnityEngine.UI.Text>();
+        LevelComplete = GameObject.Find("LevelComplete").GetComponent<UnityEngine.UI.RawImage>();
+        LevelComplete.enabled = false;
+        RoundNum.text = "Round = " + currentRound;
+        Score.text = "Correct Kittens:" + currentCorrect;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Vector3 Velocity = new Vector3( movementSpeed*Time.deltaTime, 0, 0);
-        this.transform.position = this.transform.position + Velocity;
+        Velocity = new Vector3( movementSpeed*Time.deltaTime, 0, 0);
+        if (moving)
+            this.transform.position = this.transform.position + Velocity;
 
         ButtonPress();
 	}
@@ -23,11 +42,21 @@ public class Player : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             if (hitBush){
-                     CurrentBush.AnimalFound();
-            hitBush = false;
+                BushCheck();
+                hitBush = false;
             }
 
         }
+    }
+
+    void BushCheck()
+    {
+        CurrentBush.AnimalFound();
+        if (!CurrentBush.Fox)
+            currentCorrect++;
+
+        RoundNum.text = "Round = " + currentRound;
+        Score.text = "Correct Kittens:" + currentCorrect;
     }
 
     void OnTriggerEnter(Collider hit)
@@ -37,6 +66,13 @@ public class Player : MonoBehaviour {
         {
             hitBush=true;
             CurrentBush = hit.gameObject.GetComponent<Bush>();
+        }
+        if (hit.gameObject.tag == "Finish")
+        {
+            moving = false;
+            overallCorrect += currentCorrect;
+            currentRound++;
+            LevelComplete.enabled = true;
         }
     }
     void OnTriggerExit(Collider hit)
