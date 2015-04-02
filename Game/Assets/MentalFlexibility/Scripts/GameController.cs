246f581f416ba8b58cab3acfe4b5991017361bb6
sidthree6 <psiddhu001@gmail.com>
2015-04-02 12:48:25 -0400

    Mental Game Finished!
    Only art part is left, everything else is done.

--- Game/Assets/MentalFlexibility/Scripts/GameController.cs
+++ Game/Assets/MentalFlexibility/Scripts/GameController.cs
@@ -57,6 +57,18 @@ public class GameController : MonoBehaviour {
 	private string[] curr_difficulty;
 	private int[] curr_turn_specifier;
 
+	public Slider diff_slider;
+
+	private bool game_paused = false; // Is game paused?
+
+	public GameObject paused_screen;
+	public GameObject finalScreen;
+	public Text acc;
+	public Text mean;
+	public Button reload;
+
+	private float[] meanTime = new float[60];
+
 	// Use this for initialization
 	void Start () {
 		for (int i = 0; i < panels.Length; i++) {
@@ -74,139 +86,178 @@ public class GameController : MonoBehaviour {
 
 		curr_diff_int = 3;
 
+		diff_slider.value = curr_diff_int;
+
 		score_counter.text = "" + correct_ans + "/" + max_turns;
 
 		curr_difficulty = new string[] {"First","Second","Third","Fourth"};
 		curr_turn_specifier = new int[] {10,8,6,4};
 
 		response.SetActive (false);
+
+		paused_screen.SetActive (false);
+		finalScreen.SetActive(false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		bool question_answered = false;
-		bool correct = true;
-		// NO - To are they the same
 
-		DisplayDifficulty();
-
-		SetDifficulty ();
-
-		if(turn_count < max_turns && !game_complete && !waiting){
-
-			//Debug.Log (currTime);
-			if (Input.GetKeyDown (KeyCode.LeftArrow)) {
-				// These are all the same and essentially checks what the type is
-				// in this instance if the type is colour find the colour of the first panel
-				// then check all of the rest of the panels against that and see if there is one
-				// that is the same... if there is one then tell the game this and continue
-				if(curr_dim_type == DType.COLOUR){
-					Color panel_colour = panels[0].color;
-					for(int i = 1; i < panels.Length; i++){
-						if(panels[i].color == panel_colour){
-							correct = false;
-							break;
-						}
-					}
-				}
-				else{
-					Sprite panel_image = panels[0].sprite;
-					for(int i = 1; i < panels.Length; i++){
-						if(panels[i].sprite == panel_image){
-							correct = false;
-							break;
-						}
-					}
-				}
-				question_answered = true;
-			}
-			// YES - To are they the same
-			else if (Input.GetKeyDown (KeyCode.RightArrow)) {
-				if(curr_dim_type == DType.COLOUR){
-					Color panel_colour = panels[0].color;
-					for(int i = 1; i < panels.Length; i++){
-						if(panels[i].color != panel_colour){
-							correct = false;
-							break;
-						}
-					}
-				}
-				else{
-					Sprite panel_image = panels[0].sprite;
-					for(int i = 1; i < panels.Length; i++){
-						if(panels[i].sprite != panel_image){
-							correct = false;
-							break;
-						}
-					}
-				}
-				question_answered = true;
-			}
-
-			if(currTime >= TotalTime)
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			if(game_paused)
 			{
-				//Debug.Log (currTime);				
-				timedout = true;
-				correct = false;
-				question_answered = true;
-			} 
-
-			if (question_answered == false) {
-				currTimeF += Time.deltaTime;
-				currTime = (int)currTimeF;
-			} else {
-				currTimeF = 0.0f;
-				currTime = (int)currTimeF;
+				game_paused = false;
+				paused_screen.SetActive (false);
 			}
-
-			// if the question has been answered
-			if (question_answered == true){
-				if(correct){
-					answer_field.text = "Correct";
-					correct_ans++;
-					score_counter.text = "" + correct_ans + "/" + max_turns;
-				} 
-				else{
-					if(timedout == true)
-					{
-						answer_field.text = "Timed Out";
-						timedout = false;
-					}
-					else
-					{
-						answer_field.text = "Incorrect";
-						ChangeDiff(false); // Decrease level
-					}
-				}
-
-				waiting = true;
-
-			}
-
-			if(!waiting)
-				timer_field.text = TotalTime-currTime + " s";
-
-		}
-		else if(waiting == true)
-		{
-			response.SetActive (true);
-			waitingTimeF += Time.deltaTime;
-			timer_field.text = "- s";
-			if(waitingTimeF >= 1.0f)
+			else
 			{
-				response.SetActive (false);
-				moveToNextTurn ();
-				moveToNextDimension();
-				waiting = false;
-				waitingTimeF = 0.0f;
+				game_paused = true;
+				paused_screen.SetActive (true);
 			}
 		}
-		// should execute in same frame to compensate for last turn change
-		if(turn_count >= max_turns){
-			turn_count = max_turns;
-			turn_counter.text = "" + turn_count + "/" + max_turns;
-			game_complete = true;
-		}
+
+		if (game_paused == false) {
+						bool question_answered = false;
+						bool correct = true;
+						// NO - To are they the same				
+
+						DisplayDifficulty ();
+
+						SetDifficulty ();
+
+						curr_diff_int = (int)diff_slider.value; // Change the value on Slider
+
+						if (turn_count < max_turns && !game_complete && !waiting) {
+
+								//Debug.Log (currTime);
+								if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+										// These are all the same and essentially checks what the type is
+										// in this instance if the type is colour find the colour of the first panel
+										// then check all of the rest of the panels against that and see if there is one
+										// that is the same... if there is one then tell the game this and continue
+										if (curr_dim_type == DType.COLOUR) {
+												Color panel_colour = panels [0].color;
+												for (int i = 1; i < panels.Length; i++) {
+														if (panels [i].color == panel_colour) {
+																correct = false;
+																break;
+														}
+												}
+										} else {
+												Sprite panel_image = panels [0].sprite;
+												for (int i = 1; i < panels.Length; i++) {
+														if (panels [i].sprite == panel_image) {
+																correct = false;
+																break;
+														}
+												}
+										}
+										question_answered = true;
+								}
+				// YES - To are they the same
+				else if (Input.GetKeyDown (KeyCode.RightArrow)) {
+										if (curr_dim_type == DType.COLOUR) {
+												Color panel_colour = panels [0].color;
+												for (int i = 1; i < panels.Length; i++) {
+														if (panels [i].color != panel_colour) {
+																correct = false;
+																break;
+														}
+												}
+										} else {
+												Sprite panel_image = panels [0].sprite;
+												for (int i = 1; i < panels.Length; i++) {
+														if (panels [i].sprite != panel_image) {
+																correct = false;
+																break;
+														}
+												}
+										}
+										question_answered = true;
+								}
+
+								if (currTime >= TotalTime) {
+										//Debug.Log (currTime);				
+										timedout = true;
+										correct = false;
+										question_answered = true;
+								} 
+
+								if (question_answered == false) {
+										currTimeF += Time.deltaTime;
+										currTime = (int)currTimeF;
+								} else {
+										meanTime[turn_count] = Mathf.Round(currTimeF * 100.0f)/100.0f; // Record current time in mean time array.
+										currTimeF = 0.0f;
+										currTime = (int)currTimeF;
+								}
+
+								// if the question has been answered
+								if (question_answered == true) {
+										if (correct) {
+												answer_field.text = "Correct";
+												correct_ans++;
+												score_counter.text = "" + correct_ans + "/" + max_turns;
+										} else {
+												if (timedout == true) {
+														answer_field.text = "Timed Out";
+														timedout = false;
+												} else {
+														answer_field.text = "Incorrect";
+														ChangeDiff (false); // Decrease level
+												}
+										}
+										
+										waiting = true;
+
+								}						
+								
+								if (!waiting)
+										timer_field.text = TotalTime - currTime + " s";
+
+						} else if (waiting == true) {
+								response.SetActive (true);
+								waitingTimeF += Time.deltaTime;
+								timer_field.text = "- s";
+								if (waitingTimeF >= 1.0f) {
+										response.SetActive (false);
+										moveToNextTurn ();
+										moveToNextDimension ();
+										waiting = false;
+										waitingTimeF = 0.0f;
+								}
+						}						
+
+						// should execute in same frame to compensate for last turn change
+						if (turn_count >= max_turns) {
+								turn_count = max_turns;
+								turn_counter.text = "" + turn_count + "/" + max_turns;
+								game_complete = true;
+								finalScreen.SetActive(true);
+								float totalTime = 0.0f;
+
+								for(int i=0;i<meanTime.Length;i++)
+								{
+									totalTime += meanTime[i];
+								}
+					
+								acc.text  = Mathf.Round((correct_ans*100)/max_turns) + "%";
+								mean.text = Mathf.Round((totalTime/meanTime.Length)*100.0f)/100.0f + "s";
+
+				reload.onClick.AddListener(delegate() {
+					reloadGame();
+			});
+
+					//print ("Accuracy: " + Mathf.Round((correct_ans*100)/max_turns) + "%");
+					//			print ("Mean Time: "+ Mathf.Round((totalTime/meanTime.Length)*100.0f)/100.0f + "s");
+
+						}
+									
+				}
+	}
+
+	void reloadGame()
+	{
+		Application.LoadLevel (Application.loadedLevel);
 	}
 
 	void SetDifficulty()
@@ -239,6 +290,7 @@ public class GameController : MonoBehaviour {
 			if(curr_diff_int > 0)
 				curr_diff_int--;
 		}
+		diff_slider.value = curr_diff_int;
 	}
 
 	// randomizes the images and colours, and counts turns
